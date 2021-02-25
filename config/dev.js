0b29822219acaacb9de7093a4fb93d9f5dc2eec8
coderB <flack319@gmail.com>
2021-02-25 13:14:34 +0900

    express set : cookie, session

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,12 @@
 
 module.exports = {
     // server port 
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+
+    // secret 
+    secrets: {
+        cookie: process.env.COOKIE_SECRET,
+        session: process.env.SESSION_SECRET
+    }
 }
+
--- loaders/express.js
+++ loaders/express.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const passport = require('passport');
+
+const config = require('../config');
 
 module.exports = (app) => {
     app.set('view engine', 'ejs');
 
     app.use(express.static('public'));
+
+    app.use(cookieParser(config.secrets.cookie));
+
+    app.use(expressSession({
+        secret: config.secrets.session,
+        resave: true,
+        saveUninitialized: false
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     app.use('/', (req, res) => {
         res.render('index');
