98423c0f601ffe498ec94daf2b44579afd4eb586
coderB <flack319@gmail.com>
2021-02-27 13:55:59 +0900

    google oauth

--- passport/google.js
+++ passport/google.js
@@ -2,8 +2,6 @@ const GoogleStorategy = require('passport-google-oauth20').Strategy;
 
 const config = require('../config');
 module.exports = (passport) => {
-    console.log(config.oauths.google.id);
-    console.log(config.oauths.google.secret);
     passport.use(new GoogleStorategy({
         clientID: config.oauths.google.id,
         clientSecret: config.oauths.google.secret,
