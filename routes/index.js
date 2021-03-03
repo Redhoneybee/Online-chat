94c31ebc605535e6da73cc50923d8b48c0fb5caf
coderB <flack319@gmail.com>
2021-03-03 16:58:48 +0900

    auth service class

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const AuthService = require('../services/authService')();
+const AuthService = require('../services/authService');
 
 const router = express.Router();
 
@@ -8,10 +8,9 @@ router.get('/', async (req, res) => {
     if (req.user) {
         //    user 
         // AuthService.setUser(req.user);
-        await AuthService.setUserInDB(req.user);
-        const user = JSON.stringify(AuthService.getUser());
-
-        console.log(user);
+        const AuthServiceInstance = new AuthService();
+        await AuthServiceInstance.create(req.user);
+        const user = JSON.stringify(AuthServiceInstance.getUser());
         return res.render('index', { user });
     } else {
         //   user isn't...
