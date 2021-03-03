c42d37aebd5108cc6d96966c9c0751b15b61391e
coderB <flack319@gmail.com>
2021-03-03 17:00:07 +0900

    action

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -1,6 +1,6 @@
 const actionCreateBtn = document.getElementById('action_create');
 const actionAttendBtn = document.getElementById('action_attend');
-
+const actionOKBtn = document.getElementById('action_OK')
 const createAction = async (e) => {
     e.preventDefault();
 
@@ -13,8 +13,36 @@ const createAction = async (e) => {
 }
 
 const attendAction = async (e) => {
+    e.preventDefault();
+
+    const roomCode = document.querySelector('.roomcode').value;
+
+    const { data: { url } } = await axios.get('/room/attend', { params: { roomCode } });
+
+    if (url) {
+        window.location.href += url;
+    }
 
 }
 
-actionCreateBtn.addEventListener('click', createAction);
-actionAttendBtn.addEventListener('click', attendAction);
+const okAction = async (e) => {
+    e.preventDefault();
+
+    const password = document.querySelector('#room_OK .password').value;
+
+    const { data } = await axios.post(`/room/${roomCode}/ok`, { password });
+
+
+    if (data) {
+        const { message } = data;
+        if (message === RES_OK_SUCCESSED) {
+            initInputs();
+            initRealTimer();
+        }
+    }
+
+}
+
+if (actionCreateBtn) actionCreateBtn.addEventListener('click', createAction);
+if (actionAttendBtn) actionAttendBtn.addEventListener('click', attendAction);
+if (actionOKBtn) actionOKBtn.addEventListener('click', okAction);
