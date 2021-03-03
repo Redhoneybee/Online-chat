5fab207e8b0f98989ef7be573299c3bc41b6e06c
coderB <flack319@gmail.com>
2021-03-03 16:55:10 +0900

    types

--- public/javascripts/types.js
+++ public/javascripts/types.js
@@ -1,8 +1,16 @@
 const VISIBLE = "visible";
 const ACTIVE = "active";
 
+
 const CREATE = "create";
 const ATTEND = "attend";
+const OK = "OK";
 
 const FATIMESCIRCLE = `<i class="fas fa-times-circle"></i>`;
 const FACHECKCIRCLE = `<i class="fas fa-check-circle"></i>`;
+
+
+//  response message
+const RES_OK_SUCCESSED = "res_ok_successed";
+const RES_OC_FAILED_P = "res_ok_failed_password";
+const RES_OK_NOT_FOUND = "res_ok_not_found";
