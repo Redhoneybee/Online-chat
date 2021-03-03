56251b9ae1b7a9db5787b547fb3d3f92bd97270a
coderB <flack319@gmail.com>
2021-03-03 16:57:20 +0900

    Authservice change : base function -> base class

--- services/authService.js
+++ services/authService.js
@@ -1,73 +1,88 @@
 const UserModel = require('../models/user');
 
-function AuthService() {
-    const privateInfo = {
-        username: "",
-        oauth: {
-            provider: "",
-            id: ""
-        },
-        photo: "",
-    };
+class AuthService {
+    // private member
+    #user;
 
-    const accesser = {
-        setUser: (user) => {
-            privateInfo.username = user.username;
-            privateInfo.photo = user.photo;
-            privateInfo.oauth.provider = user.oauthprovider;
-            privateInfo.oauth.id = user.oauthid;
-        },
-        getUser: () => privateInfo,
-        getProvider: () => privateInfo.oauth.provider,
-        getOauthId: () => privateInfo.oauth.id,
-
-        findUserInDB: async function (_provider, _oauthid) {
-            try {
-                const user = await UserModel.findOne({ oauthprovider: _provider, oauthid: _oauthid });
-
-                if (user === null || user === undefined) {
-                    //  not found...
-                    return null;
-                } else {
-                    // found...
-                    return user;
-                }
-
-            } catch (err) {
-                throw new Error("Couldn't find usermodel in mongodb, " + err);
-            }
-        },
-
-
-        setUserInDB: async function (user) {
-            try {
-                const { provider } = user;
-                const { sub } = user._json;
-
-
-                let recode = await this.findUserInDB(provider, sub);
-
-                if (!recode) {
-                    // isn't...
-                    recode = await UserModel.create({
-                        username: user._json.name,
-                        oauthprovider: provider,
-                        oauthid: sub,
-                        photo: user._json.picture,
-                    });
-
-                }
-                this.setUser(recode);
-            } catch (err) {
-                throw new Error("Couldn' set User model in mongodb, " + err);
-            }
-        },
-
+    constructor() {
+        this.#user = {
+            _id: "",
+            oauth: {
+                provider: "",
+                id: ""
+            },
+            photo: ""
+        };
     }
 
-    return accesser;
+    setUser(user) {
+        this.#user._id = user._id;
+        this.#user.username = user.username;
+        this.#user.oauth.provider = user.oauthprovider;
+        this.#user.oauth.id = user.oauthid;
+        this.#user.photo = user.photo;
+    };
+
+    getUser = () => this.#user;
+    getProvider = () => this.#user.oauth.provider;
+    getOauthId = () => this.#user.oauth.id;
+
+    async searchOauthInDB(_provider, _oauthid) {
+        try {
+            const user = await UserModel.findOne({ oauthprovider: _provider, oauthid: _oauthid });
+
+            if (user === null || user === undefined) {
+                //  not found...
+                return false;
+            } else {
+                // found...
+                this.setUser(user);
+                return true;
+            }
+
+        } catch (err) {
+            throw err;
+        }
+    };
+    async searchIdInDB(_id) {
+        try {
+            const user = await UserModel.findOne({ _id });
+            if (user === null || user === undefined) {
+                //  not found...
+                return false;
+            } else {
+                // found...
+                this.setUser(user);
+                return true;
+            }
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    async create(user) {
+        try {
+            const { provider } = user;
+            const { name, picture, sub } = user._json;
+
+
+            const result = await this.searchOauthInDB(provider, sub);
+            let recode = undefined;
+
+            if (!result) {
+                // isn't...
+                recode = await UserModel.create({
+                    username: name,
+                    oauthprovider: provider,
+                    oauthid: sub,
+                    photo: picture,
+                });
+                this.setUser(recode);
+            }
+        } catch (err) {
+            throw err;
+        }
+    };
 }
 
-module.exports = () => {
-    return AuthService();
-}
+module.exports = AuthService;
