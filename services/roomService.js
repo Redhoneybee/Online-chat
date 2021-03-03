fd7b40b58341c9cdcfd3ffd5b8d9ea3b4d93c1f0
coderB <flack319@gmail.com>
2021-03-03 16:57:50 +0900

    room service

---
+++ services/roomService.js
@@ -0,0 +1,76 @@
+const AuthService = require('./authService');
+
+const { v4: uuidV4 } = require('uuid');
+const { randomBytes } = require('crypto');
+const argon2 = require('argon2');
+
+const RoomModel = require('../models/room');
+
+class roomService {
+    #owner;
+    #room;
+
+    constructor() {
+        this.#owner = null;
+        this.#room = null;
+    };
+
+    setOwner(user) {
+        this.#owner = user;
+    }
+    getOwner = () => this.#owner;
+    setRoom(room) {
+        this.#room = room;
+    }
+    getRoom = () => this.#room;
+    getRoomCode = () => this.#room.roomCode;
+
+    static isValiedPassword(password, compare) {
+        if (!password || !compare) return false;
+        else if (typeof password !== "string" || typeof compare !== "string") return false;
+        else if (password !== compare) return false;
+        else return true;
+    };
+
+    async create(password, compare) {
+        if (roomService.isValiedPassword(password, compare)) {
+            const salt = randomBytes(32);
+            const passwordHash = await argon2.hash(password, salt);
+
+            const roomCode = uuidV4();
+            const { _id } = this.#owner;
+
+            const recode = await RoomModel.create({
+                roomCode,
+                password: passwordHash,
+                salt: salt.toString('hex'),
+                owner: _id
+            });
+            this.#room = recode;
+        } else {
+            throw new Error("Couldn't create new room");
+        }
+    };
+
+    async isValidNotOwner(password) {
+        return await argon2.verify(this.#room.password, password);
+    }
+
+    async searchInDB(roomCode) {
+        const AuthServiceInstance = new AuthService();
+        console.log(typeof roomCode, roomCode);
+        const room = await RoomModel.findOne({ roomCode });
+        console.log(room);
+        if (!room) return false;
+        else this.setRoom(room);
+
+        const result = await AuthServiceInstance.searchIdInDB(this.#room.owner);
+
+        if (result) this.setOwner(AuthServiceInstance.getUser());
+        else return false;
+
+        return true;
+    };
+}
+
+module.exports = roomService;
